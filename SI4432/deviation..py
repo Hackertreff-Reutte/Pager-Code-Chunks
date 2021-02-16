0a22cb5e59a595ab6b3122e8de7416f3b693ac00
UnHold (Alex) <alex@hold.re>
2021-02-16 22:43:13 +0100

    removed some duplicated files and fixed the name of a file

--- SI4432/deviation..py
+++
@@ -1,9 +0,0 @@
-deviation = float(input("Devication: "))
-
-if deviation < 1 or deviation > 320:
-    print("Deviation out of range!\n")
-    exit()
-
-fd = round(deviation / 0.625)
-
-print("Shift: " + str(fd) + "   HEX: " + hex(fd) + "  BIN: " + bin(fd))
--- SI4432/offset.py
+++
@@ -1,8 +0,0 @@
-offset = float(input("Offset: "))
-
-
-hbsel = 0
-
-fo = round(offset / (0.15625 * (hbsel + 1)))
-
-print("Offset: " + str(fo) + "   HEX: " + hex(fo) + "  BIN: " + bin(fo))
